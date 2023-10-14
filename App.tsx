80ff92e1405f38cb8dbee2cb974d5554e2982fab
nolotus <s@nolotus.com>
2023-10-14 16:50:23 +0800

    ssl test

--- App.tsx
+++ App.tsx
@@ -1,34 +1,33 @@
-import React, { useState ,useMemo} from "react";
-import {unified} from 'unified';
-import remarkParse from 'remark-parse';
-import remarkGfm from 'remark-gfm';
-import {renderMdastNode} from './renderMdastNode';
+import React, { useState, useMemo } from "react";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import { renderMdastNode } from "./renderMdastNode";
 
-
-export default function App({context}) {
-
-  console.log('text',context.text)
-const text =context.text
+export default function App({ context }) {
+  const text = context.text;
   const mdast = useMemo(() => {
     const processor = unified().use(remarkParse).use(remarkGfm);
     return processor.parse(text);
   }, [text]);
   const renderedContent = useMemo(() => {
-    return renderMdastNode(mdast, '1');
+    return renderMdastNode(mdast, "1");
   }, [mdast]);
 
   return (
     <html>
       <head>
-      <script dangerouslySetInnerHTML={{ __html: `window.NOLO_STORE_DATA=${JSON.stringify(text)}` }}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.NOLO_STORE_DATA=${JSON.stringify(text)}`,
+          }}
+        ></script>
         <meta charSet="utf-8" />
         <title>Bun, Elysia & React</title>
         <meta name="description" content="Bun, Elysia & React" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>
-        {renderedContent}
-      </body>
+      <body>{renderedContent}</body>
     </html>
   );
 }
--- packages/server/index.tsx
+++ packages/server/index.tsx
@@ -1,37 +1,65 @@
 import { createElement } from "react";
 import { renderToReadableStream } from "react-dom/server";
-
 import App from "../../App";
 import { readFile } from "../database/read";
-
 await Bun.build({
   entrypoints: ["./packages/web/index.tsx"],
   outdir: "./public",
 });
 
 const text = await readFile();
+if (process.env.ENV === "production") {
+  Bun.serve({
+    port: process.env.PORT,
+    async fetch(req) {
+      const url = new URL(req.url);
+      if (url.pathname.startsWith("/public")) {
+        const file = url.pathname.replace("/public", "");
+        return new Response(Bun.file(`public/${file}`));
+      }
 
-Bun.serve({
-  port: 3000,
-  async fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname.startsWith("/public")) {
-      const file = url.pathname.replace("/public", "");
-      return new Response(Bun.file(`public/${file}`));
-    }
+      if (url.pathname === "/") {
+        const app = createElement(App, { context: { text } });
 
-    if (url.pathname === "/") {
-      const app = createElement(App, { context: { text } });
+        const stream = await renderToReadableStream(app, {
+          bootstrapScripts: ["/public/index.js"],
+        });
+        return new Response(stream, {
+          headers: { "Content-Type": "text/html" },
+        });
+      }
 
-      const stream = await renderToReadableStream(app, {
-        bootstrapScripts: ["/public/index.js"],
-      });
-      return new Response(stream, {
-        headers: { "Content-Type": "text/html" },
-      });
-    }
+      if (url.pathname === "/blog") return new Response("Blog!");
+      return new Response("404!");
+    },
+    tls: {
+      key: Bun.file("./key.pem"),
+      cert: Bun.file("./cert.pem"),
+    },
+  });
+} else {
+  Bun.serve({
+    port: process.env.PORT,
+    async fetch(req) {
+      const url = new URL(req.url);
+      if (url.pathname.startsWith("/public")) {
+        const file = url.pathname.replace("/public", "");
+        return new Response(Bun.file(`public/${file}`));
+      }
 
-    if (url.pathname === "/blog") return new Response("Blog!");
-    return new Response("404!");
-  },
-});
+      if (url.pathname === "/") {
+        const app = createElement(App, { context: { text } });
+
+        const stream = await renderToReadableStream(app, {
+          bootstrapScripts: ["/public/index.js"],
+        });
+        return new Response(stream, {
+          headers: { "Content-Type": "text/html" },
+        });
+      }
+
+      if (url.pathname === "/blog") return new Response("Blog!");
+      return new Response("404!");
+    },
+  });
+}
