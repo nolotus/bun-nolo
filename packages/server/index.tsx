26cc3591d0d128fcb366927441bc24739ddb1f90
nolotus <s@nolotus.com>
2023-10-14 17:40:19 +0800

    add ssl test

--- packages/server/index.tsx
+++ packages/server/index.tsx
@@ -2,15 +2,16 @@ import { createElement } from "react";
 import { renderToReadableStream } from "react-dom/server";
 import App from "../../App";
 import { readFile } from "../database/read";
-await Bun.build({
-  entrypoints: ["./packages/web/index.tsx"],
-  outdir: "./public",
-});
 
 const text = await readFile();
+
 if (process.env.ENV === "production") {
   Bun.serve({
+    port: 443,
+    hostname: "0.0.0.0",
     async fetch(req) {
+      console.log("443 req", req);
+
       const url = new URL(req.url);
       if (url.pathname.startsWith("/public")) {
         const file = url.pathname.replace("/public", "");
@@ -36,29 +37,31 @@ if (process.env.ENV === "production") {
       cert: Bun.file("./cert.pem"),
     },
   });
-} else {
-  Bun.serve({
-    port: process.env.PORT,
-    async fetch(req) {
-      const url = new URL(req.url);
-      if (url.pathname.startsWith("/public")) {
-        const file = url.pathname.replace("/public", "");
-        return new Response(Bun.file(`public/${file}`));
-      }
-
-      if (url.pathname === "/") {
-        const app = createElement(App, { context: { text } });
-
-        const stream = await renderToReadableStream(app, {
-          bootstrapScripts: ["/public/index.js"],
-        });
-        return new Response(stream, {
-          headers: { "Content-Type": "text/html" },
-        });
-      }
-
-      if (url.pathname === "/blog") return new Response("Blog!");
-      return new Response("404!");
-    },
-  });
 }
+
+Bun.serve({
+  port: 80,
+  hostname: "0.0.0.0",
+  async fetch(req) {
+    console.log("80 req", req);
+    const url = new URL(req.url);
+    if (url.pathname.startsWith("/public")) {
+      const file = url.pathname.replace("/public", "");
+      return new Response(Bun.file(`public/${file}`));
+    }
+
+    if (url.pathname === "/") {
+      const app = createElement(App, { context: { text } });
+
+      const stream = await renderToReadableStream(app, {
+        bootstrapScripts: ["/public/index.js"],
+      });
+      return new Response(stream, {
+        headers: { "Content-Type": "text/html" },
+      });
+    }
+
+    if (url.pathname === "/blog") return new Response("Blog!");
+    return new Response("404!");
+  },
+});
