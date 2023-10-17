0d57ea796a00636d906af712ceffee45faf03add
nolotus <s@nolotus.com>
2023-10-17 12:42:09 +0800

    better build

---
+++ scripts/build.ts
@@ -0,0 +1,33 @@
+import { build } from 'bun';
+
+const env = process.env.NODE_ENV;
+const isProduction = env ==='production';
+
+const commonConfig = {
+  entrypoints: ['./packages/web/entry.tsx'],
+  outdir: './public',
+  naming: "[dir]/[name].[ext]",
+  format: 'esm',
+}
+
+const productionConfig = {
+  ...commonConfig,
+  minify: {
+    whitespace: true,
+    identifiers: true,
+    syntax: true,
+  }
+}
+
+const config = isProduction ? productionConfig : commonConfig;
+
+async function runBuild() {
+  try {
+    const result = await build(config);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+runBuild();
