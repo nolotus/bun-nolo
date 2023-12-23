47ec51daa3cc88661831281244c1c6cc71c2b91e
nolotus <s@nolotus.com>
2023-12-23 14:04:14 +0800

    release pass

--- packages/rn/App.tsx
+++ packages/rn/App.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import { StatusBar, useColorScheme } from "react-native";
 
 import { Colors } from "react-native/Libraries/NewAppScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import "intl-pluralrules";
+
 import i18n from "i18n";
 import * as RNLocalize from "react-native-localize";
-import "intl-pluralrules";
 
 import { Provider } from "react-redux";
 import { mobileStore } from "./store";
 import MainNavigation from "./MainNavigation";
-const Tab = createBottomTabNavigator();
-// 请根据具体需求调整颜色值
 
 function App(): React.JSX.Element {
 	const isDarkMode = useColorScheme() === "dark";
