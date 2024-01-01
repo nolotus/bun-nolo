ee1a7a4824927e9a79a059ce1b1158c125936c0a
nolotus <s@nolotus.com>
2024-01-01 16:30:50 +0800

    new spots page

--- packages/app/pages/Spots.tsx
+++ packages/app/pages/Spots.tsx
@@ -4,32 +4,28 @@ import { DataType } from "create/types";
 import { useGetEntriesQuery } from "database/services";
 import React from "react";
 import { SpotCard } from "render/components/SpotCard";
+const options = {
+	isJSON: true,
+	condition: {
+		type: DataType.SurfSpot,
+	},
+	limit: 20,
+};
+const renderSpotList = (spots) => {
+	if (!spots) {
+		return null;
+	}
 
+	const filteredSpots = spots.filter((spot) => !spot.is_template);
+
+	return filteredSpots.map((spot) => <SpotCard key={spot.id} data={spot} />);
+};
 const Spots = () => {
-	const options = {
-		isJSON: true,
-		condition: {
-			type: DataType.SurfSpot,
-		},
-		limit: 20,
-	};
-
 	const { data, error, isLoading, isSuccess } = useGetEntriesQuery({
 		userId: nolotusId,
 		options,
 	});
 	// 定义函数来渲染条目列表，过滤掉 is_template 的项
-	const renderSpotList = (spots) => {
-		if (!spots) {
-			return null;
-		}
-
-		// 过滤出 is_template 不为 true 的项
-		const filteredSpots = spots.filter((spot) => spot.is_template !== true);
-
-		// 映射过滤后的数组元素到 JSX
-		return filteredSpots.map((spot) => <SpotCard data={spot} />);
-	};
 
 	// 根据加载状态渲染组件
 	if (isLoading) {
@@ -48,7 +44,7 @@ const Spots = () => {
           新增浪点
         </button> */}
 			</div>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			<div className="flex flex-wrap -mx-2">
 				{isSuccess && renderSpotList(data)}
 			</div>
 		</>
---
+++ packages/database/server/writeHash.ts
@@ -0,0 +1,83 @@
+import { promises as fs } from "fs";
+import { createWriteStream } from "node:fs";
+import { dirname } from "path";
+import { pipeline, Readable } from "stream";
+import { promisify } from "util";
+
+import { formatData } from "core/formatData";
+import { generateKey } from "core/generateMainKey";
+import { nolotusId } from "core/init";
+import { DataType } from "create/types";
+// import {WriteDataRequestBody} from '../types';
+
+export const handleError = (res, error) => {
+	console.error(error);
+	const status = error.message === "Access denied" ? 401 : 500;
+	return res.status(status).json({ error: error.message });
+};
+
+const pipelineAsync = promisify(pipeline);
+
+const allowType = {
+	[nolotusId]: [DataType.TokenStatistics],
+};
+
+export const writeData = async (dataKey, data, userId) => {
+	const path = `./nolodata/${userId}/hash.nolo`;
+	try {
+		await fs.access(dirname(path));
+	} catch {
+		throw new Error("没有该用户");
+	}
+	const output = createWriteStream(path, { flags: "a" });
+	await pipelineAsync(Readable.from(`${dataKey} ${data}\n`), output);
+};
+
+export const handleWriteHash = async (req, res) => {
+	const { userId, data, flags, customId } = req.body;
+	if (flags.isHash) {
+		const { user } = req;
+		const actionUserId = user.userId;
+
+		const saveUserId = userId;
+
+		const isWriteSelf = actionUserId === saveUserId;
+		const value = formatData(data, flags);
+
+		if (value.includes("\n")) {
+			return res.status(400).json({
+				message: "Data contains newline character and is not allowed.",
+			});
+		}
+		const dataId = generateKey(value, saveUserId, flags, customId);
+
+		if (isWriteSelf) {
+			try {
+				await writeData(dataId, value, saveUserId);
+				return res.status(200).json({
+					message: "Data written to file successfully.",
+					dataId,
+				});
+			} catch (error) {
+				return handleError(res, error);
+			}
+		} else {
+			const userRule = allowType[saveUserId];
+			const isAllowType = userRule?.includes(data.type);
+
+			if (isAllowType) {
+				await writeData(dataId, value, saveUserId);
+				return res.status(200).json({
+					message: "Data written to file successfully.",
+					dataId,
+				});
+			}
+			return res.status(403).json({
+				message: "操作不被允许.",
+			});
+		}
+	}
+	return res.status(403).json({
+		message: "操作不被允许.",
+	});
+};
