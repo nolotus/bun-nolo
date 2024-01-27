aae3a19cef0881b00a5c7d41916c37ca81515202
nolotus <s@nolotus.com>
2024-01-27 13:14:36 +0800

    add gpt-4-0125-preview

--- packages/integrations/openAI/index.ts
+++
@@ -1,6 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-	apiKey: process.env.OPENAI_KEY, // It's recommended to use environment variable for your API key
-});
-export const openai = new OpenAIApi(configuration);
