4dc06ea5c8fad1d52f329d1257c858350f775158
nolotus <s@nolotus.com>
2024-03-14 13:00:28 +0800

    change to create.reducer

--- packages/chat/chatSlice.ts
+++ packages/chat/chatSlice.ts
@@ -1,77 +1,83 @@
 import {
-	createSlice,
-	PayloadAction,
-	createEntityAdapter,
+  createSlice,
+  PayloadAction,
+  createEntityAdapter,
+  buildCreateSlice,
+  asyncThunkCreator,
 } from "@reduxjs/toolkit";
 import { RootState } from "app/store";
 
 import { ChatConfig } from "./types";
 
 export type ChatSliceState = {
-	chatList: ReturnType<typeof chatAdapter.getInitialState>;
-	currentChatId: string | null;
+  chatList: ReturnType<typeof chatAdapter.getInitialState>;
+  currentChatId: string | null;
 };
-
+const createAppSlice = buildCreateSlice({
+  creators: { asyncThunk: asyncThunkCreator },
+});
 export const chatAdapter = createEntityAdapter<ChatConfig>({
-	// Assume IDs are stored in the 'id' field of each chat config
-	selectId: (chat) => chat.id,
+  // Assume IDs are stored in the 'id' field of each chat config
+  selectId: (chat) => chat.id,
 });
 export const {
-	selectAll: selectAllChats,
-	selectById: selectChatById, // 新添加的按ID选择实体的选择器
-	// 可以继续添加其他需要的选择器，例如 selectIds, selectEntities, selectTotal
+  selectAll: selectAllChats,
+  selectById: selectChatById, // 新添加的按ID选择实体的选择器
+  // 可以继续添加其他需要的选择器，例如 selectIds, selectEntities, selectTotal
 } = chatAdapter.getSelectors((state: RootState) => state.chat.chatList);
 
-export const chatSlice = createSlice({
-	name: "chat",
-	initialState: {
-		currentChatId: null,
-		chatList: chatAdapter.getInitialState(),
-	},
-	reducers: {
-		fetchChatListSuccess: (
-			state: ChatSliceState,
-			action: PayloadAction<any[]>,
-		) => {
-			chatAdapter.upsertMany(state.chatList, action.payload);
-			if (!state.currentChatId) {
-				state.currentChatId = action.payload[0].id
-					? action.payload[0].id
-					: null;
-			}
-		},
+export const chatSlice = createAppSlice({
+  name: "chat",
 
-		fetchDefaultConfig: (
-			state: ChatSliceState,
-			action: PayloadAction<ChatConfig>,
-		) => {
-			state.currentChatId = action.payload.id;
-			chatAdapter.upsertOne(state.chatList, action.payload);
-		},
+  initialState: {
+    currentChatId: null,
+    chatList: chatAdapter.getInitialState(),
+  },
 
-		reloadChatList: (
-			state: ChatSliceState,
-			action: PayloadAction<ChatConfig[]>,
-		) => {
-			chatAdapter.setAll(state.chatList, action.payload);
-		},
-		updateChatConfig: (
-			state: ChatSliceState,
-			action: PayloadAction<{ id: string; changes: ChatConfig }>,
-		) => {
-			chatAdapter.updateOne(state.chatList, {
-				id: action.payload.id,
-				changes: action.payload,
-			});
-		},
-	},
+  reducers: (create) => ({
+    fetchChatListSuccess: create.reducer(
+      (state: ChatSliceState, action: PayloadAction<any[]>) => {
+        chatAdapter.upsertMany(state.chatList, action.payload);
+        if (!state.currentChatId) {
+          state.currentChatId = action.payload[0].id
+            ? action.payload[0].id
+            : null;
+        }
+      },
+    ),
+
+    fetchDefaultConfig: create.reducer(
+      (state: ChatSliceState, action: PayloadAction<ChatConfig>) => {
+        state.currentChatId = action.payload.id;
+        chatAdapter.upsertOne(state.chatList, action.payload);
+      },
+    ),
+
+    reloadChatList: create.reducer(
+      (state: ChatSliceState, action: PayloadAction<ChatConfig[]>) => {
+        chatAdapter.setAll(state.chatList, action.payload);
+      },
+    ),
+
+    updateChatConfig: create.reducer(
+      (
+        state: ChatSliceState,
+        action: PayloadAction<{ id: string; changes: ChatConfig }>,
+      ) => {
+        chatAdapter.updateOne(state.chatList, {
+          id: action.payload.id,
+          changes: action.payload,
+        });
+      },
+    ),
+  }),
 });
 
 export const {
-	fetchChatListSuccess,
-	fetchDefaultConfig,
-	reloadChatList,
-	updateChatConfig,
+  fetchChatListSuccess,
+  fetchDefaultConfig,
+  reloadChatList,
+  updateChatConfig,
 } = chatSlice.actions;
 
 export default chatSlice.reducer;
