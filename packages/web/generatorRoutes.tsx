ea214910b68baf4397cfa775622532b4ca6e5cba
nolotus <s@nolotus.com>
2024-04-29 23:38:20 +0800

    add kr.nolotus.com

--- packages/web/generatorRoutes.tsx
+++ packages/web/generatorRoutes.tsx
@@ -1,9 +1,9 @@
-import { chatRoutes } from 'chat/route';
-import React, { Suspense, lazy } from 'react';
+import { chatRoutes } from "chat/route";
+import React, { Suspense, lazy } from "react";
 
 // import { routes as UIRoutes } from "../ui/route";
 
-import { settingRoutes } from 'setting/route';
+import { settingRoutes } from "setting/route";
 // import { lifeRoutes } from "../domain/life/route";
 // import { createRoutes } from "../domain/create/route";
 
@@ -16,8 +16,9 @@ import { settingRoutes } from 'setting/route';
 const hostRoutesMap = {
   // "nolotus.test": yujierRoutes,
   // "nolotus.xyz": yujierRoutes,
-  'nolotus.local': nolotusRoutes,
-  'nolotus.com': nolotusRoutes,
+  "nolotus.local": nolotusRoutes,
+  "nolotus.com": nolotusRoutes,
+  "kr.nolotus.com": nolotusRoutes,
   // "nolotus.top": uniqeicRoutes,
 };
 
