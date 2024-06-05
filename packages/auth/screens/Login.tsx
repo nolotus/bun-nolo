0e141141a7045f81da3e42abd35ff6ca776fc506
nolotus <s@nolotus.com>
2024-06-05 18:49:26 +0800

    react native argon2 test

--- packages/auth/screens/Login.tsx
+++ packages/auth/screens/Login.tsx
@@ -1,60 +1,139 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TextInput, Button, Text } from "react-native";
+import { View, StyleSheet, TextInput, Button, Text, Alert } from "react-native";
 import * as RNLocalize from "react-native-localize";
 import { storeTokens } from "auth/client/token";
 import { Picker } from "@react-native-picker/picker";
 import { useAppDispatch } from "app/hooks";
 import { signIn } from "../authSlice";
+import { useForm, Controller } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import Argon2 from "@sphereon/react-native-argon2";
 
+console.log("Argon2", Argon2);
 const LoginScreen = ({ navigation }) => {
   const dispatch = useAppDispatch();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [selectedAlgorithm, setSelectedAlgorithm] = useState("argon2");
+  // const dispatch = useAppDispatch();
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [selectedAlgorithm, setSelectedAlgorithm] = useState("argon2");
+  // const handleLogin = async () => {
 
-  const handleLogin = async () => {
+  //   const input = { username, password, locale };
+  //   const resultAction = await dispatch(signIn(input));
+  //   if (resultAction.payload.token) {
+  //     storeTokens(resultAction.payload.token);
+  //     navigation.navigate("User"); // 跳转到 AuthStack 的 Login Screen
+  //   }
+  // };
+  // return (
+  //   <View style={styles.container}>
+
+  //     <TextInput
+  //       style={styles.input}
+  //       placeholder="请输入密码"
+  //       value={password}
+  //       onChangeText={setPassword}
+  //       secureTextEntry // 隐藏密码文字
+  //     />
+  //     <Picker
+  //       selectedValue={selectedAlgorithm}
+  //       style={{ height: 50, width: 150 }}
+  //       onValueChange={(itemValue, itemIndex) =>
+  //         setSelectedAlgorithm(itemValue)
+  //       }
+  //     >
+  //       <Picker.Item label="Argon2" value="argon2" />
+  //       <Picker.Item label="Scrypt" value="scrypt" />
+  //     </Picker>
+  //     <Button title="登录" onPress={handleLogin} />
+  //   </View>
+  // );
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+    // const { t } = useTranslation();
+
     const deviceLanguage = RNLocalize.getLocales()[0].languageCode;
     const deviceCountry = RNLocalize.getCountry(); // 'US', 'CN' 等
     const locale = `${deviceLanguage}-${deviceCountry}`;
-    const input = { username, password, locale };
-    const resultAction = await dispatch(signIn(input));
-    if (resultAction.payload.token) {
-      storeTokens(resultAction.payload.token);
-      navigation.navigate("User"); // 跳转到 AuthStack 的 Login Screen
-    }
+    dispatch(signIn({ ...data, locale }))
+      .then(() => {
+        navigation.navigate("User"); // 跳转到 AuthStack 的 Login Screen
+        // navigate(`/${LifeRoutePaths.WELCOME}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        // let message;
+        // switch (noloError.message) {
+        //   case "404":
+        //     message = t("errors.userNotFound");
+        //     break;
+        //   case "403":
+        //     message = t("errors.invalidCredentials");
+        //     break;
+        //   case "400":
+        //     message = t("errors.validationError");
+        //     break;
+        //   case "500":
+        //   default:
+        //     message = t("errors.serverError");
+        //     break;
+        // }
+        // setError(message);
+      });
   };
-
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="请输入用户名"
-        value={username}
-        onChangeText={setUsername}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="请输入用户名"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="username"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="请输入密码"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry // 隐藏密码文字
+      {errors.username && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="请输入密码"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry // 隐藏密码文字
+          />
+        )}
+        name="password"
       />
-      <Picker
-        selectedValue={selectedAlgorithm}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedAlgorithm(itemValue)
-        }
-      >
-        <Picker.Item label="Argon2" value="argon2" />
-        <Picker.Item label="Scrypt" value="scrypt" />
-      </Picker>
-      <Button title="登录" onPress={handleLogin} />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 };
 
-//这里可以根据实际需要设计样式
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -66,7 +145,6 @@ const styles = StyleSheet.create({
     width: "100%",
     marginVertical: 10,
     borderWidth: 1,
-    borderColor: "gray",
     padding: 10,
   },
 });
