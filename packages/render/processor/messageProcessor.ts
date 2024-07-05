872de48d63959b4609692146052823500aaeb574
nolotus <s@nolotus.com>
2024-07-06 00:08:23 +0800

    message use math

--- packages/render/processor/messageProcessor.ts
+++ packages/render/processor/messageProcessor.ts
@@ -2,5 +2,9 @@ import { unified } from "unified";
 
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
 
-export const messageProcessor = unified().use(remarkParse).use(remarkGfm);
+export const messageProcessor = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkMath);
