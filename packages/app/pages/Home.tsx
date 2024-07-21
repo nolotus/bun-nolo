4e2cbacfff67cf312271ef6f605ceede79200906
nolotus <s@nolotus.com>
2024-07-21 22:47:55 +0800

    fix home error

--- packages/app/pages/Home.tsx
+++ packages/app/pages/Home.tsx
@@ -1,4 +1,4 @@
--import { Link } from "render/ui";
+import { Link } from "render/ui";
 import { nolotusId } from "core/init";
 
 import ChatAIList from "ai/blocks/ChatAIList";
@@ -17,69 +17,62 @@ const Home = () => {
 
   return (
     <div>
-      <div className="flex flex-col gap-6 lg:flex-row">
-        <div
-          className="text2 mb-6 w-full lg:mb-0 lg:w-1/2"
-          style={{ display: "grid", gap: "var(--size-fluid-2)" }}
-        >
-          <h2>嘿，你好,我是Nolotus！</h2>
-          <p>
-            我在这里用AI管理我的一切。
-            <br />
-            从日常的Todos，到旅途拍摄的美景。
-          </p>
-          <p>
-            <div className="py-3">
-              无论是收集
-              <Link to="/spots">旅居点</Link>
-              的信息，
-              <br />
-              亦或是
-              <Link to="/spots">各种兴趣爱好 </Link>
-              的攻略。
-              <br />
-              例如
-              <Link to="/spots">冲浪</Link>和<Link to="/spots">滑雪</Link>。
-            </div>
-            <div className="py-3">
-              甚至是管理我的这些兴趣爱好的
-              <Link to="/gears" className="text-blue-500">
-                装备
-              </Link>
-              。
-              <br />
-              总而言之我用它管理我的一切数据，并分享给来到这里的你。
-            </div>
-          </p>
-        </div>
-        <aside
-          className="surface2 rad-shadow w-full lg:w-1/2 "
-          style={{
-            display: "grid",
-            gap: "var(--size-fluid-2)",
-            padding: "var(--size-fluid-2)",
-            boxShadow: "var(--shadow-3)",
-          }}
-        >
-          <h3>
-            如果你也想分享, 点击{" "}
-            <Link to="/signup" className="font-bold underline decoration-wavy">
-              注册
-            </Link>{" "}
-            使用
-          </h3>
-          <p>
-            可以用AI直接对话来管理你的一切 无论照片，文字，声音，视频，
-            <br />
-            可以绑定你的个人域名,甚至可以搭建你的私有服务
-          </p>
-          <p>
-            还可以进一步， 认识更多的朋友 通过
-            <Link to="/itineraries">一起出发</Link>，
-            <Link to="/people">寻找你周边的人</Link>，
-            <Link to="/people">寻找相同兴趣爱好的人</Link>，
-          </p>
-        </aside>
+      <div
+        className="flex flex-col gap-6"
+        style={{ gap: "var(--size-fluid-1)" }}
+      >
+        <h2>嘿，你好，我是Nolotus！</h2>
+        <h2>嗨，欢迎来到这里！👋，本站还在测试中!</h2>
+        <h3>很多功能都不完善，如果你有任何问题请发送邮件至s@nolotus.com</h3>
+        <p>
+          我在这里用AI <DependabotIcon size="medium" />
+          管理我的一切,从日常的Todo，财务，到旅途拍摄的美景。
+        </p>
+        <ChatAIList queryUserId={userId ? userId : nolotusId} limit={9} />
+        <h3>
+          分享旅居路上各种信息
+          <LocationIcon size="medium" />
+        </h3>
+        <p>
+          无论是
+          <Link to="/spots">旅居点</Link>， 亦或是
+          <Link to="/spots">各种兴趣爱好 </Link>
+          的攻略。 例如<Link to="/spots">冲浪</Link>和
+          <Link to="/spots">滑雪</Link>。
+        </p>
+        <SpotList userId={nolotusId} />
+        <h4>如果你也想分享你的生活,用AI管理你的数据</h4>
+        <h3>
+          请
+          <Link to="/signup" className="font-bold underline decoration-wavy">
+            注册
+          </Link>{" "}
+          使用吧！
+        </h3>
+        <p>
+          可以用AI直接对话来管理你的一切 无论照片，文字，声音，视频，
+          <br />
+          他们可以只存在你的本地，也可以同步多个设备和系统，并且从保存那一刻就加密
+          <br />
+          还可以绑定你的个人域名,甚至通过开源代码搭建你的私有服务
+        </p>
+        {/* <h3>
+          这里不仅仅有我，还有我的朋友 <PeopleIcon size="medium" />
+        </h3>
+        <p>
+          我们可以一起出发旅行，也可以认识你附近的人，或者寻找跟你相同爱好的人
+        </p> */}
+        {/* <p>
+          甚至是管理我的这些兴趣爱好的
+          <Link to="/gears" className="text-blue-500">
+            装备
+          </Link>
+          。
+          <br />
+        </p> */}
+        {/* 
+ 
+  */}
       </div>
     </div>
   );
