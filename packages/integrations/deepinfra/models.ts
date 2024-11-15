5c1bf038ef05bfe51b73574f5f8d53a54329dfad
nolotus <s@nolotus.com>
2024-11-15 22:52:44 +0800

    add qwen2.5 model

--- packages/integrations/deepinfra/models.ts
+++ packages/integrations/deepinfra/models.ts
@@ -4,4 +4,16 @@ export const deepinfraModels = {
     output: 0.4,
     contextLength: "32k",
   },
+  "Qwen/Qwen2.5-Coder-32B-Instruct": {
+    input: 0.18,
+    output: 0.18,
+    contextLength: "32k",
+  },
+  "meta-llama/Meta-Llama-3.1-405B-Instruct": {
+    input: 1.79,
+    output: 1.79,
+    contextLength: "32k",
+    contextWindow: "32768",
+    speed: "20",
+  },
 };
--- packages/integrations/fireworks/models.ts
+++ packages/integrations/fireworks/models.ts
@@ -2,6 +2,17 @@ export const fireworksmodels = {
   "accounts/fireworks/models/llama-v3p1-405b-instruct": {
     input: 3,
     output: 3,
-    contextLength: "32k",
+    contextLength: "131072",
+    speed: 73,
+  },
+  "accounts/fireworks/models/qwen2p5-coder-32b-instruct": {
+    input: 0.9,
+    output: 0.9,
+    contextLength: "32768",
+  },
+  "accounts/fireworks/models/qwen2p5-72b-instruct": {
+    input: 0.9,
+    output: 0.9,
+    contextLength: "32768",
   },
 };
