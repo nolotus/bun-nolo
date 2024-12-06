954c34aa519ae6ed93fa4b588ea84a19b0a8def6
nolotus <s@nolotus.com>
2024-12-06 13:57:02 +0800

    add HOTKEYS

--- packages/create/editor/ExampleToolbar.tsx
+++ packages/create/editor/ExampleToolbar.tsx
@@ -75,7 +75,7 @@ const toggleBlock = (editor, format, ordered = undefined) => {
   }
 };
 
-const toggleMark = (editor, format) => {
+export const toggleMark = (editor, format) => {
   const isActive = isMarkActive(editor, format);
   if (isActive) {
     Editor.removeMark(editor, format);
--- packages/create/editor/useOnKeyDown.tsx
+++ packages/create/editor/useOnKeyDown.tsx
@@ -1,16 +1,38 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { Editor } from "slate";
+import { toggleMark } from "./ExampleToolbar";
+
+const HOTKEYS = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+};
+
+// 简单的热键判断实现
+const isHotkey = (hotkey, event) => {
+  const [modifier, key] = hotkey.split("+");
+  const isMod = modifier === "mod" && (event.metaKey || event.ctrlKey);
+  return isMod && event.key.toLowerCase() === key;
+};
 
 export const useOnKeydown = (editor) => {
   const onKeyDown = useCallback(
     (e) => {
+      for (const hotkey in HOTKEYS) {
+        if (isHotkey(hotkey, e)) {
+          e.preventDefault();
+          const mark = HOTKEYS[hotkey];
+          toggleMark(editor, mark);
+        }
+      }
+
       if (e.key === "Tab") {
-        // 直接判断key属性
         e.preventDefault();
         Editor.insertText(editor, "  ");
       }
     },
     [editor],
   );
+
   return onKeyDown;
 };
