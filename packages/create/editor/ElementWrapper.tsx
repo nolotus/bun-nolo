385756137f0ce147980e78b42382b4fd88de06fa
nolotus <s@nolotus.com>
2024-12-19 22:47:11 +0800

    new Table

--- packages/create/editor/ElementWrapper.tsx
+++ packages/create/editor/ElementWrapper.tsx
@@ -1,196 +1,196 @@
-import React from "react";
-import { useSlateStatic } from "slate-react";
-import { CodeBlockType, CodeLineType } from "./type";
-import { Table, TableRow, TableCell } from "render/elements/Table";
-import { List, ListItem } from "render/elements/List";
 import { useAppSelector } from "app/hooks";
 import { selectTheme } from "app/theme/themeSlice";
-import { SafeLink } from "render/elements/SafeLink";
-import { ImageElement } from "render/elements/ImageElement";
+import React from "react";
 import { CodeBlock } from "render/elements/CodeBlock";
+import { ImageElement } from "render/elements/ImageElement";
+import { List, ListItem } from "render/elements/List";
+import { SafeLink } from "render/elements/SafeLink";
+import { Table, TableCell, TableRow } from "render/elements/Table";
+import { useSlateStatic } from "slate-react";
+import { CodeBlockType, CodeLineType } from "./type";
 
 export const ElementWrapper = (props) => {
-  const { attributes, children, element, isDarkMode } = props;
-  const editor = useSlateStatic();
-  const theme = useAppSelector(selectTheme);
-  const style = { textAlign: element.align }; // 添加基础样式对象
+	const { attributes, children, element, isDarkMode } = props;
+	const editor = useSlateStatic();
+	const theme = useAppSelector(selectTheme);
+	const style = { textAlign: element.align }; // 添加基础样式对象
 
-  // 处理代码块
-  if (element.type === CodeBlockType) {
-    return (
-      <CodeBlock
-        attributes={attributes}
-        element={element}
-        children={children}
-      />
-    );
-  }
+	// 处理代码块
+	if (element.type === CodeBlockType) {
+		return (
+			<CodeBlock
+				attributes={attributes}
+				element={element}
+				children={children}
+			/>
+		);
+	}
 
-  if (element.type === CodeLineType) {
-    return (
-      <div {...attributes} style={{ position: "relative", ...style }}>
-        {children}
-      </div>
-    );
-  }
+	if (element.type === CodeLineType) {
+		return (
+			<div {...attributes} style={{ position: "relative", ...style }}>
+				{children}
+			</div>
+		);
+	}
 
-  // 处理其他元素类型
-  switch (element.type) {
-    case "paragraph":
-      return (
-        <p {...attributes} style={style}>
-          {children}
-        </p>
-      );
+	// 处理其他元素类型
+	switch (element.type) {
+		case "paragraph":
+			return (
+				<p {...attributes} style={style}>
+					{children}
+				</p>
+			);
 
-    // 标题
-    case "heading-one":
-      return (
-        <h1 {...attributes} style={style}>
-          {children}
-        </h1>
-      );
-    case "heading-two":
-      return (
-        <h2 {...attributes} style={style}>
-          {children}
-        </h2>
-      );
-    case "heading-three":
-      return (
-        <h3 {...attributes} style={style}>
-          {children}
-        </h3>
-      );
-    case "heading-four":
-      return (
-        <h4 {...attributes} style={style}>
-          {children}
-        </h4>
-      );
-    case "heading-five":
-      return (
-        <h5 {...attributes} style={style}>
-          {children}
-        </h5>
-      );
-    case "heading-six":
-      return (
-        <h6 {...attributes} style={style}>
-          {children}
-        </h6>
-      );
+		// 标题
+		case "heading-one":
+			return (
+				<h1 {...attributes} style={style}>
+					{children}
+				</h1>
+			);
+		case "heading-two":
+			return (
+				<h2 {...attributes} style={style}>
+					{children}
+				</h2>
+			);
+		case "heading-three":
+			return (
+				<h3 {...attributes} style={style}>
+					{children}
+				</h3>
+			);
+		case "heading-four":
+			return (
+				<h4 {...attributes} style={style}>
+					{children}
+				</h4>
+			);
+		case "heading-five":
+			return (
+				<h5 {...attributes} style={style}>
+					{children}
+				</h5>
+			);
+		case "heading-six":
+			return (
+				<h6 {...attributes} style={style}>
+					{children}
+				</h6>
+			);
 
-    // 引用
-    case "quote":
-      return (
-        <blockquote
-          {...attributes}
-          style={{
-            borderLeft: `4px solid ${theme.borderColor}`,
-            margin: "1.5em 0",
-            padding: "0.5em 0 0.5em 1em",
-            backgroundColor: theme.surface2,
-            textAlign: element.align,
-          }}
-        >
-          {children}
-        </blockquote>
-      );
+		// 引用
+		case "quote":
+			return (
+				<blockquote
+					{...attributes}
+					style={{
+						borderLeft: `4px solid ${theme.borderColor}`,
+						margin: "1.5em 0",
+						padding: "0.5em 0 0.5em 1em",
+						backgroundColor: theme.surface2,
+						textAlign: element.align,
+					}}
+				>
+					{children}
+				</blockquote>
+			);
 
-    // 链接和图片
-    case "link":
-      return (
-        <SafeLink href={element.url} {...attributes} style={style}>
-          {children}
-        </SafeLink>
-      );
-    case "image":
-      return <ImageElement {...props} style={style} />;
+		// 链接和图片
+		case "link":
+			return (
+				<SafeLink href={element.url} {...attributes} style={style}>
+					{children}
+				</SafeLink>
+			);
+		case "image":
+			return <ImageElement {...props} style={style} />;
 
-    // 列表相关
-    case "list":
-      return (
-        <List
-          attributes={attributes}
-          element={element}
-          theme={theme}
-          style={style}
-        >
-          {children}
-        </List>
-      );
+		// 列表相关
+		case "list":
+			return (
+				<List
+					attributes={attributes}
+					element={element}
+					theme={theme}
+					style={style}
+				>
+					{children}
+				</List>
+			);
 
-    case "list-item":
-      return (
-        <ListItem
-          attributes={attributes}
-          element={element}
-          theme={theme}
-          editor={editor}
-          style={style}
-        >
-          {children}
-        </ListItem>
-      );
+		case "list-item":
+			return (
+				<ListItem
+					attributes={attributes}
+					element={element}
+					theme={theme}
+					editor={editor}
+					style={style}
+				>
+					{children}
+				</ListItem>
+			);
 
-    // 表格相关
-    case "table":
-      return (
-        <Table attributes={attributes} theme={theme} style={style}>
-          {children}
-        </Table>
-      );
-    case "table-row":
-      return (
-        <TableRow attributes={attributes} theme={theme} style={style}>
-          {children}
-        </TableRow>
-      );
-    case "table-cell":
-      return (
-        <TableCell
-          attributes={attributes}
-          element={element}
-          theme={theme}
-          style={style}
-        >
-          {children}
-        </TableCell>
-      );
+		// 表格相关
+		case "table":
+			return (
+				<Table attributes={attributes} theme={theme} style={style}>
+					{children}
+				</Table>
+			);
+		case "table-row":
+			return (
+				<TableRow attributes={attributes} theme={theme} style={style}>
+					{children}
+				</TableRow>
+			);
+		case "table-cell":
+			return (
+				<TableCell
+					attributes={attributes}
+					element={element}
+					theme={theme}
+					style={style}
+				>
+					{children}
+				</TableCell>
+			);
 
-    // 分割线
-    case "thematic-break":
-      return (
-        <hr
-          {...attributes}
-          style={{
-            border: "none",
-            borderBottom: `1px solid #000`,
-            margin: "1em 0",
-            ...style,
-          }}
-        />
-      );
+		// 分割线
+		case "thematic-break":
+			return (
+				<hr
+					{...attributes}
+					style={{
+						border: "none",
+						borderBottom: `1px solid #000`,
+						margin: "1em 0",
+						...style,
+					}}
+				/>
+			);
 
-    // HTML 内容
-    case "html-block":
-    case "html-inline":
-      return (
-        <div
-          {...attributes}
-          style={style}
-          dangerouslySetInnerHTML={{ __html: element.html }}
-        />
-      );
+		// HTML 内容
+		case "html-block":
+		case "html-inline":
+			return (
+				<div
+					{...attributes}
+					style={style}
+					dangerouslySetInnerHTML={{ __html: element.html }}
+				/>
+			);
 
-    // 默认处理
-    default:
-      const Tag = editor.isInline(element) ? "span" : "div";
-      return (
-        <Tag {...attributes} style={{ position: "relative", ...style }}>
-          {children}
-        </Tag>
-      );
-  }
+		// 默认处理
+		default:
+			const Tag = editor.isInline(element) ? "span" : "div";
+			return (
+				<Tag {...attributes} style={{ position: "relative", ...style }}>
+					{children}
+				</Tag>
+			);
+	}
 };
--- packages/render/elements/Table.tsx
+++ packages/render/elements/Table.tsx
@@ -1,91 +1,126 @@
-// elements/Table.tsx
-import React, { useState } from "react";
+import { defaultTheme } from "render/styles/colors";
 
 interface TableBaseProps {
-  attributes: any;
-  children: React.ReactNode;
-  theme: any;
+	attributes?: any;
+	children: React.ReactNode;
+	theme?: typeof defaultTheme;
+	style?: React.CSSProperties;
 }
 
-const getTableStyle = (theme: any) => ({
-  borderCollapse: "collapse" as const,
-  width: "100%",
-  margin: "1em 0",
-  background: theme.table.background,
-  color: theme.table.color,
-});
-
-export const Table: React.FC<TableBaseProps> = ({
-  attributes,
-  children,
-  theme,
-}) => (
-  <table style={getTableStyle(theme)} {...attributes}>
-    {children}
-  </table>
-);
-
-const getRowStyle = (theme: any, isHovered: boolean) => ({
-  borderBottom: `1px solid ${theme.row.borderColor}`,
-  background: isHovered ? theme.row.hoverBackground : theme.row.background,
-  transition: "background-color 0.2s ease",
-});
-
-export const TableRow: React.FC<TableBaseProps> = ({
-  attributes,
-  children,
-  theme,
-}) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <tr
-      style={getRowStyle(theme, isHovered)}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      {...attributes}
-    >
-      {children}
-    </tr>
-  );
+// 利用defaultTheme定义表格主题
+const tableTheme = {
+	table: {
+		background: defaultTheme.background,
+		color: defaultTheme.text,
+		borderRadius: "8px",
+		boxShadow: `0 1px 3px ${defaultTheme.shadowLight}`,
+	},
+	row: {
+		background: defaultTheme.background,
+		hoverBackground: defaultTheme.backgroundSecondary,
+		borderColor: defaultTheme.border,
+	},
+	header: {
+		background: defaultTheme.backgroundSecondary,
+		color: defaultTheme.textSecondary,
+		borderColor: defaultTheme.border,
+		fontSize: "0.9375rem",
+	},
+	cell: {
+		padding: "14px 16px",
+		borderColor: defaultTheme.borderLight,
+		color: defaultTheme.text,
+		fontSize: "0.875rem",
+	},
 };
 
+export const Table: React.FC<TableBaseProps> = ({
+	attributes,
+	children,
+	theme = tableTheme,
+	style,
+}) => (
+	<table
+		style={{
+			borderCollapse: "separate",
+			borderSpacing: 0,
+			width: "100%",
+			margin: "1.5em 0",
+			background: theme.table.background,
+			color: theme.table.color,
+			borderRadius: theme.table.borderRadius,
+			boxShadow: `0 1px 3px ${defaultTheme.shadowLight}`,
+			...style,
+		}}
+		{...attributes}
+	>
+		{children}
+	</table>
+);
+
+export const TableRow: React.FC<TableBaseProps> = ({
+	attributes,
+	children,
+	theme = tableTheme,
+	style,
+}) => (
+	<>
+		<style>
+			{`
+        .table-row {
+          border-bottom: 1px solid ${defaultTheme.border};
+          background: ${defaultTheme.background};
+          transition: background-color 0.15s ease;
+        }
+        .table-row:hover {
+          background: ${defaultTheme.backgroundSecondary};
+        }
+      `}
+		</style>
+		<tr className="table-row" style={style} {...attributes}>
+			{children}
+		</tr>
+	</>
+);
+
 interface TableCellProps extends TableBaseProps {
-  element: {
-    header?: boolean;
-  };
+	element: {
+		header?: boolean;
+	};
 }
 
-const getCellStyle = (theme: any, isHeader: boolean) =>
-  isHeader
-    ? {
-        backgroundColor: theme.header.background,
-        color: theme.header.color,
-        fontWeight: 600,
-        padding: theme.cell.padding,
-        textAlign: "left" as const,
-        border: `1px solid ${theme.header.borderColor}`,
-      }
-    : {
-        padding: theme.cell.padding,
-        border: `1px solid ${theme.cell.borderColor}`,
-      };
-
 export const TableCell: React.FC<TableCellProps> = ({
-  attributes,
-  children,
-  element,
-  theme,
+	attributes,
+	children,
+	element,
+	theme = tableTheme,
+	style,
 }) => {
-  const cellStyle = getCellStyle(theme, element.header);
+	const Component = element.header ? "th" : "td";
 
-  return element.header ? (
-    <th style={cellStyle} {...attributes}>
-      {children}
-    </th>
-  ) : (
-    <td style={cellStyle} {...attributes}>
-      {children}
-    </td>
-  );
+	const cellStyles = {
+		padding: theme.cell.padding,
+		fontSize: theme.cell.fontSize,
+		lineHeight: 1.6,
+		letterSpacing: "0.01em",
+		color: defaultTheme.text,
+		borderBottom: `1px solid ${defaultTheme.border}`,
+		verticalAlign: "middle",
+		...(element.header && {
+			backgroundColor: defaultTheme.backgroundSecondary,
+			color: defaultTheme.textSecondary,
+			fontSize: theme.header.fontSize,
+			fontWeight: 600,
+			height: "48px",
+			textAlign: "left" as const,
+			whiteSpace: "nowrap" as const,
+		}),
+		...style,
+	};
+
+	return (
+		<Component style={cellStyles} {...attributes}>
+			{children}
+		</Component>
+	);
 };
