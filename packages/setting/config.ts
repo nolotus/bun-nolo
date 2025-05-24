75e31bc8e7481df5a39b2d0a690a639f24c3314a
bin zhang <bin@codelessuniverse.com>
2025-05-24 19:59:02 +0800

    feat: Add Chat Configuration settings and routing

--- packages/setting/config.ts
+++ packages/setting/config.ts
@@ -1,8 +1,8 @@
 // setting/config.tx
-
 export const SettingRoutePaths = {
   SETTING: "setting",
   SETTING_USER_PROFILE: "user-profile",
   SETTING_EDITOR_CONFIG: "editor-config",
   SETTING_CUSTOMIZE: "customize",
+  SETTING_CHAT_CONFIG: "chat-config", // 新增对话设置路径
 };
