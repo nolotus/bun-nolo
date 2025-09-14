f72152d432f9ee2ade483dfad03a0642acfdc463
nolotus <s@nolotus.com>
2025-09-15 01:11:16 +0800

    set default language to Chinese (ZH_CN) in i18n configuration

--- packages/app/i18n/index.ts
+++ packages/app/i18n/index.ts
@@ -7,7 +7,7 @@ import { i18nConfig } from "./i18n.config"; // <-- 从共享配置导入
 
 i18n.use(initReactI18next).init({
   ...i18nConfig, // <-- 使用共享配置
-  lng: Language.EN, // 客户端可以有一个默认语言
+  lng: Language.ZH_CN, // 客户端可以有一个默认语言
 });
 
 export default i18n;
